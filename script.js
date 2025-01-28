05401cc35ed9f0f26889ca1f35d2f4307130fc42
vaibhavrajharsh <harshrajj8804@gmail.com>
2025-01-28 23:39:54 +0530

    update

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ async function getSongs(folder) {
   currFolder = folder;
   let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
   let response = await a.text();
+  //console.log(response)
   let div = document.createElement("div");
   div.innerHTML = response;
   let list = div.getElementsByTagName("a");
